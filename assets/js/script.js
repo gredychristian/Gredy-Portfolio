57b931d67db1bcab01b24ee5cc40a3cdacd05a89
Gredy Christian <gredy24chris@gmail.com>
2025-01-01 14:49:09 +0700

    Major update on web pages including styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-// For future development
+// Function to load external HTML content into an element
+function loadHTML(file, elementId) {
+  fetch(file)
+    .then(response => {
+      // Jika file ditemukan (status 200 OK)
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error('File not found');
+    })
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error loading HTML:', error);
+    });
+}
+
+// Load header and footer content
+document.addEventListener('DOMContentLoaded', function () {
+  loadHTML('assets/header.html', 'header-container');
+  loadHTML('assets/footer.html', 'footer-container');
+});
